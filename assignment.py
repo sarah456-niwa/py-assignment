84620ca347149823710f3396ea048752ed74e5fa
sarah456-niwa <snuwahereza0@gmail.com>
2024-11-11 22:44:29 +0300

    first commit

---
+++ assignment.py
@@ -0,0 +1,24 @@
+#create a class with the following.
+# class, Cohort. these are the things you need from them.
+#name, description, start-date, end-date
+#Add a constructor for the class
+class Cohort_four:
+        name= 'Niwahereza Sarafina'
+        description='Female at WITU'
+        start_date = "20/08/2024"
+        end_date = "20/12/2024"
+cohort = Cohort_four()
+print(cohort.name) 
+print(cohort.description)  
+print(cohort.start_date)
+print(cohort.end_date)     
+
+#Create a method/function to the class that prints the cohort name and the total number of students.
+class Cohort4:
+ def _init_(students, name , total_number_of_students):
+    students.name = name 
+    students.total_number_of_students= total_number_of_students
+cohort= Cohort4('Niwahereza Sarafina', 267)
+print(cohort.name)
+print(cohort.total_number_of_students)
+#Create a new instance of the cohort class.
